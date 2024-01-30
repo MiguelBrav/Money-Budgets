3cdedaef12a1f0e3820ace5df9520a1007f856f2
MiguelBrav <miguel_98-@hotmail.com>
2024-01-29 22:48:39 -0600

    Add razor project and first form

---
+++ MoneyBudgets/Controllers/AccountTypeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyBudgets.Models;
+using System.Diagnostics;
+
+namespace MoneyBudgets.Controllers
+{
+    public class AccountTypeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AccountTypeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AccountTypeModel account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(account);
+
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ MoneyBudgets/Models/AccountTypeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyBudgets.Models
+{
+    public class AccountTypeModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public int Order { get; set; }
+  
+    }
+}
